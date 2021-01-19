ec832bdfb4bab0a97bae32b616c7a985068aaf43
alexbyz <huber.alexander1993@gmail.com>
2021-01-19 07:46:11 +0100

    wordcloud

--- createWorldcloud.py
+++ createWorldcloud.py
@@ -1,19 +1,30 @@
-#from wordcloud import WordCloud
-#import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import json, yaml
+import functions
 
-# def createWordCloud(savePath, tfIdfDic):
-#     wc = WordCloud(width=1000, height=600, background_color="white", random_state=2,
-#                    relative_scaling=0.5, colormap="gray") 
-#     wc.generate_from_frequencies(tfIdfDic)
-#     # plotting
-#     plt.imshow(wc, interpolation="bilinear")
-#     plt.axis("off")
-#     #plt.show() # this line will show the plot
-#     plt.savefig(savePath, dpi=200, bbox_inches='tight')
+settingsFile = "./settings.yml"
+settings = yaml.load(open(settingsFile))
 
-def loadTfIdfFile(filename):
-    with open(filename, "r", encoding="utf8") as f1:
-        loaded = f1.read()
-        print(type(loaded))
+memexPath = settings["path_to_memex"]
 
-loadTfIdfFile("tfidfTableDic_filtered.txt")
+def createWordCloud(savePath, tfIdfDic):
+    wc = WordCloud(width=1000, height=600, background_color="white", random_state=2,
+                   relative_scaling=0.5, colormap="gray") 
+    wc.generate_from_frequencies(tfIdfDic)
+    # plotting
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    #plt.show() # this line will show the plot
+    plt.savefig(savePath, dpi=200, bbox_inches='tight')
+
+def createAll(filename):
+    docData = json.load(open(filename, "r", encoding="utf8"))
+
+    for k, v in docData.items():
+        savePath = functions.generatePublPath(memexPath, k)
+        savePath = savePath + "\\" + k
+        if v:
+            createWordCloud(savePath, v)
+
+createAll("tfidfTableDic_filtered.txt")
